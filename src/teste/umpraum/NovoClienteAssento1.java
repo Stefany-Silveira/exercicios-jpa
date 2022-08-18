4a53268959d4b12f674a1de12f28c530b9b29ed6
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-18 18:00:41 -0300

    add @ Um pra Um

---
+++ src/teste/umpraum/NovoClienteAssento1.java
@@ -0,0 +1,22 @@
+package src.teste.umpraum;
+
+import infra.DAO;
+import modelo.umpraum.Assento;
+import modelo.umpraum.Cliente;
+
+public class NovoClienteAssento1 {
+
+    public static void main(String[] args) {
+
+        Assento assento = new Assento("23F");
+        Cliente cliente = new Cliente("Bia", assento);
+
+        DAO<Object> dao = new DAO<>();
+
+        dao.abrirT()
+                .incluir(cliente)
+                .incluir(assento)
+                .fecharT()
+                .fechar();
+    }
+}
