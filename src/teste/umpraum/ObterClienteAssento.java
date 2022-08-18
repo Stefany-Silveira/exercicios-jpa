2eef0db740a000d6bd6934480fa5766f589a3632
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-18 18:52:11 -0300

    add @ Um pra Um #03

---
+++ src/teste/umpraum/ObterClienteAssento.java
@@ -0,0 +1,25 @@
+package src.teste.umpraum;
+
+import infra.DAO;
+import modelo.umpraum.Assento;
+import modelo.umpraum.Cliente;
+
+public class ObterClienteAssento {
+
+    public static void main(String[] args) {
+
+        DAO< Cliente> dao = new DAO<>(Cliente.class);
+
+        Cliente cliente = dao.obterPorID(1L);
+        System.out.println(cliente.getAssento().getNome());
+
+        dao.fechar();
+
+        DAO<Assento> daoAssento = new DAO<>(Assento.class);
+
+        Assento assento = daoAssento.obterPorID(4L);
+        System.out.println(assento.getCliente().getNome());
+
+        daoAssento.fechar();
+    }
+}
